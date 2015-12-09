500c525e654e096cbe1e3756f9533b6a4164af10
Rui Gonçalves <ruippeixotog@gmail.com>
2015-12-09 23:51:04 +0000

    Add solutions to Day 9 problems

---
+++ day9_1.cpp
@@ -0,0 +1,59 @@
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+#define MAXC 50
+#define INF 10e8
+
+using namespace std;
+
+map<string, int> ids;
+int graph[MAXC][MAXC];
+
+istream& readEntry(string& city1, string& city2, int& dist) {
+  string word;
+  return cin >> city1 >> word >> city2 >> word >> dist;
+}
+
+int getOrAdd(string& city) {
+  if(ids.count(city)) return ids[city];
+  int id = ids[city] = ids.size();
+  return id;
+}
+
+bool visited[MAXC];
+
+int dfs(int k, int curr, int dist) {
+  if(k == ids.size()) return dist;
+
+  int best = INF;
+  for(int i = 0; i < ids.size(); i++) {
+    if(!visited[i] && graph[curr][i] > 0) {
+      visited[i] = true;
+      best = min(best, dfs(k + 1, i, dist + graph[curr][i]));
+      visited[i] = false;
+    }
+  }
+  return best;
+}
+
+int main() {
+  string city1, city2; int dist;
+
+  while(readEntry(city1, city2, dist)) {
+    int c1 = getOrAdd(city1);
+    int c2 = getOrAdd(city2);
+    graph[c1][c2] = graph[c2][c1] = dist;
+  }
+
+  int best = INF;
+  for(int i = 0; i < ids.size(); i++) {
+    visited[i] = true;
+    best = min(best, dfs(1, i, 0));
+    visited[i] = false;
+  }
+
+  cout << best << endl;
+  return 0;
+}
