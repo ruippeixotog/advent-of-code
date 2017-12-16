b2c62d5f7230e3d40d02358b337f064ffe51fd20
Rui Gonçalves <ruippeixotog@gmail.com>
2017-12-16 14:26:59 +0000

    Add solution to Day 14 problems

--- 2017/day14.cpp
+++ 2017/day14.cpp
@@ -1,14 +1,19 @@
 #include <cstdio>
 #include <iostream>
-#include <iomanip>
 #include <utility>
 #include <vector>
 
 #define N 256
 #define ROUNDS 64
 
+#define GRID (N / 2)
+
 using namespace std;
 
+bool grid[GRID][GRID];
+bool visited[GRID][GRID];
+
+// copy of day10_2.cpp
 vector<int> knotHash(string lenghts) {
   lenghts.insert(lenghts.end(), { 17, 31, 73, 47, 23 });
 
@@ -37,11 +42,32 @@ vector<int> knotHash(string lenghts) {
   return hash;
 }
 
+int ffill(int i, int j) {
+  if(i < 0 || i >= GRID || j < 0 || j >= GRID || visited[i][j] || !grid[i][j]) {
+    return 0;
+  }
+  visited[i][j] = true;
+  return 1 + ffill(i - 1, j) + ffill(i + 1, j) +
+    ffill(i, j - 1) + ffill(i, j + 1);
+}
+
 int main() {
   string input; cin >> input;
-  for(int byte : knotHash(input)) {
-    cout << setw(2) << setfill('0') << hex << byte;
+  for(int i = 0; i < GRID; i++) {
+    vector<int> hash = knotHash(input + "-" + to_string(i));
+    for(int j = 0; j < 16; j++) {
+      for(int k = 0; k < 8; k++) {
+        grid[i][j * 8 + k] = (hash[j] & (1 << (7 - k))) > 0;
+      }
+    }
   }
-  cout << endl;
+  int cnt = 0, groupCnt = 0;
+  for(int i = 0; i < GRID; i++) {
+    for(int j = 0; j < GRID; j++) {
+      int sz = ffill(i, j);
+      if(sz > 0) { cnt += sz; groupCnt++; }
+    }
+  }
+  printf("%d %d\n", cnt, groupCnt);
   return 0;
 }
