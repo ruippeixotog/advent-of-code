c84a44735fb2e98aea14836dc0d84867f8bad73c
Rui Gonçalves <ruippeixotog@gmail.com>
2020-11-11 00:30:05 +0000

    Add Haskell solution to 2019 Day 4

---
+++ 2019/day04.hs
@@ -0,0 +1,35 @@
+import Data.Bifunctor (bimap)
+import Data.List (unfoldr)
+
+digits :: Int -> [Int]
+digits = reverse . unfoldr (\x -> if x == 0 then Nothing else Just (mod x 10, div x 10))
+
+hasTwoAdj :: [Int] -> Bool
+hasTwoAdj (a : b : rest) = a == b || hasTwoAdj (b : rest)
+hasTwoAdj _ = False
+
+hasExactlyTwoAdj :: [Int] -> Bool
+hasExactlyTwoAdj [] = False
+hasExactlyTwoAdj (a : rest) =
+  let (prefix, rest') = span (== a) rest
+   in length prefix == 1 || hasExactlyTwoAdj rest'
+
+nonDecreasing :: [Int] -> Bool
+nonDecreasing (a : b : rest) = a <= b && nonDecreasing (b : rest)
+nonDecreasing _ = True
+
+solve1 :: (Int, Int) -> Int
+solve1 (st, end) =
+  length $ filter hasTwoAdj $ filter nonDecreasing $ map digits [st .. end]
+
+solve2 :: (Int, Int) -> Int
+solve2 (st, end) =
+  length $ filter hasExactlyTwoAdj $ filter nonDecreasing $ map digits [st .. end]
+
+parseInput :: String -> (Int, Int)
+parseInput str = bimap read (read . tail) $ break (== '-') str
+
+main :: IO ()
+main = interact $ show . tee solve1 solve2 . parseInput
+  where
+    tee f1 f2 arg = (f1 arg, f2 arg)
