45b3731912d5a7b352ae09ed48e95cc9b5885afe
Rui Gonçalves <rui@fb.com>
2020-12-15 13:44:59 +0000

    Optimize Day 15 solution

--- 2020/day15.cpp
+++ 2020/day15.cpp
@@ -1,23 +1,22 @@
 #include <cstdio>
-#include <unordered_map>
 #include <vector>
+
+#define TGT 30000000
 
 using namespace std;
 
 vector<int> initial = {9, 3, 1, 0, 8, 4};
 
-int main() {
-  unordered_map<int, vector<int>> lastK;
+int prevIdx[TGT], diff[TGT];
 
+int main() {
   int last = -1, res1 = -1;
-  for(int i = 0; i < 30000000; i++) {
+  for(int i = 0; i < TGT; i++) {
     if(i == 2020) res1 = last;
 
-    int next = i < initial.size() ? initial[i] :
-      lastK[last].size() < 2 ? 0 :
-      *lastK[last].rbegin() - *(lastK[last].rbegin() + 1);
-
-    lastK[next].push_back(i);
+    int next = i < initial.size() ? initial[i] : diff[last];
+    diff[next] = prevIdx[next] ? i + 1 - prevIdx[next] : 0;
+    prevIdx[next] = i + 1;
     last = next;
   }
   printf("%d %d\n", res1, last);
