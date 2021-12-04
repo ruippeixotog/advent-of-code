7056dba09e4804dca7f3981aff6664be57d3aeee
Rui Gonçalves <ruippeixotog@gmail.com>
2021-12-04 21:35:48 +0000

    Rename Card to Board on Day 4

--- 2021/day04.hs
+++ 2021/day04.hs
@@ -2,33 +2,33 @@ import Control.Arrow
 import Data.List
 import Data.List.Split
 
-type Card = [[Int]]
+type Board = [[Int]]
 
-play :: [Int] -> [Card] -> [Int]
+play :: [Int] -> [Board] -> [Int]
 play _ [] = []
 play [] _ = error "No more draws"
-play (d : ds) cards =
-  case partition isWinner $ updateCard <$> cards of
+play (d : ds) boards =
+  case partition isWinner $ updateBoard <$> boards of
     (winners, rest) -> map score winners ++ play ds rest
   where
-    updateCard = map $ map (\e -> if e == d then 0 else e)
-    isWinner card = elem 0 $ sum <$> card ++ transpose card
-    score card = d * sum (sum <$> card)
+    updateBoard = map $ map (\e -> if e == d then 0 else e)
+    isWinner board = elem 0 $ sum <$> board ++ transpose board
+    score board = d * sum (sum <$> board)
 
-solve1 :: ([Int], [Card]) -> Int
+solve1 :: ([Int], [Board]) -> Int
 solve1 = head . uncurry play
 
-solve2 :: ([Int], [Card]) -> Int
+solve2 :: ([Int], [Board]) -> Int
 solve2 = last . uncurry play
 
-parseInput :: String -> ([Int], [Card])
+parseInput :: String -> ([Int], [Board])
 parseInput = readInput . splitOn "\n\n"
   where
-    readInput (str : rest) = (readDraws str, readCard <$> rest)
+    readInput (str : rest) = (readDraws str, readBoard <$> rest)
     readInput _ = error "Invalid input"
 
     readDraws = map read . splitOn ","
-    readCard = map (map read . words) . lines
+    readBoard = map (map read . words) . lines
 
 main :: IO ()
 main = interact $ show . (solve1 &&& solve2) . parseInput
