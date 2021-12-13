adb71ad6c20cc8d2c980bdc2771f25014046db3c
Rui Gonçalves <ruippeixotog@gmail.com>
2021-12-13 23:37:46 +0000

    Add aliases for folds and dots in Day 13

--- 2021/day13.hs
+++ 2021/day13.hs
@@ -4,17 +4,21 @@ import Data.List.Split
 import Data.Tuple
 import Text.Regex
 
-foldDot :: (Char, Int) -> (Int, Int) -> (Int, Int)
+type Fold = (Char, Int)
+
+type Dot = (Int, Int)
+
+foldDot :: Fold -> Dot -> Dot
 foldDot ('y', y0) (x, y) = swap $ foldDot ('x', y0) (y, x)
 foldDot (_, x0) (x, y) = if x < x0 then (x, y) else (x0 - (x - x0), y)
 
-foldPaper :: (Char, Int) -> [(Int, Int)] -> [(Int, Int)]
+foldPaper :: Fold -> [Dot] -> [Dot]
 foldPaper fld = nub . map (foldDot fld)
 
-solve1 :: ([(Int, Int)], [(Char, Int)]) -> Int
+solve1 :: ([Dot], [Fold]) -> Int
 solve1 (dots, folds) = length $ foldPaper (head folds) dots
 
-solve2 :: ([(Int, Int)], [(Char, Int)]) -> String
+solve2 :: ([Dot], [Fold]) -> String
 solve2 (dots, folds) = showPaper $ foldl (flip foldPaper) dots folds
   where
     showPaper ds = intercalate "\n" $ showRow ds <$> [0 .. maximum $ snd <$> ds]
@@ -23,7 +27,7 @@ solve2 (dots, folds) = showPaper $ foldl (flip foldPaper) dots folds
         [] -> ""
         xs -> [if x `elem` xs then '#' else '.' | x <- [0 .. maximum xs]]
 
-parseInput :: String -> ([(Int, Int)], [(Char, Int)])
+parseInput :: String -> ([Dot], [Fold])
 parseInput = (map parseDot . head &&& map parseFold . last) . map lines . splitOn "\n\n"
   where
     parseDot = (head &&& last) . map read . splitOn ","
