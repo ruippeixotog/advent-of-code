f2c54a53382926e7bf942427893df590967ba76c
Rui Gonçalves <ruippeixotog@gmail.com>
2022-12-03 18:43:12 +0000

    Add solution to Day 3 problems

---
+++ 2022/day03.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+object Day03 extends App {
+  val in = Source.fromFile("day03.in").getLines.toList
+
+  def prio(ch: Char): Int =
+    if(ch >= 'a') ch - 'a' + 1 else ch - 'A' + 27
+
+  println(
+    in.map { line => line.splitAt(line.length / 2) }
+      .map { case (l, r) => prio((l.toSet & r.toSet).head) }
+      .sum
+  )
+  println(in.map(_.toSet).grouped(3).map(_.reduce(_ & _).head).map(prio).sum)
+}
