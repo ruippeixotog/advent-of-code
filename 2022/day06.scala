c4d228cc62fbb1ed5ccf6701a1f333c1de435cf4
Rui Gonçalves <ruippeixotog@gmail.com>
2022-12-06 22:11:03 +0000

    Add solution to Day 6 problems

---
+++ 2022/day06.scala
@@ -0,0 +1,8 @@
+import scala.io.Source
+
+object Day06 extends App {
+  val in = Source.fromFile("2022/day06.in").mkString
+
+  println(in.sliding(4).indexWhere(_.toSet.size == 4) + 4)
+  println(in.sliding(14).indexWhere(_.toSet.size == 14) + 14)
+}
